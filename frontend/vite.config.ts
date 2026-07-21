291ccfba6e290ed9661b021d16d1745e7bd997f6
Hari Haran P <hariharanp@ducontindia.local>
2026-07-21 15:52:12 +0530

    Configure gh pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/Stakeholder-Platform-POC',
   server: {
     port: 5173,
     strictPort: false,
